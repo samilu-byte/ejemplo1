6cc132e2c19cd410981112c456f8be9ca6889baa
samilu-byte <natalysortegap06@gmail.com>
2024-06-26 16:15:44 -0600

    tarea 1

---
+++ ejercicio1.py
@@ -0,0 +1,13 @@
+a=5
+b=5
+c=5
+d=5
+e=5
+suma= a+b+c+d+e
+resta= a-b-c-d-e
+multiplicacion= a*b*c*d*e
+division= a/b/c/d/e
+print("Suma ",suma)
+print("Resta ",resta)
+print("Multiplicacion ",multiplicacion)
+print("Division ",division)
---
+++ ejercicio2.py
@@ -0,0 +1,13 @@
+n1=int(input("Ingrese su primer dato \n"))
+n2=int(input("Ingrese su segundo dato \n"))
+n3=int(input("Ingrese su tercer dato \n"))
+n4=int(input("Ingrese su cuarto dato \n"))
+n5=int(input("Ingrese su quinto dato \n"))
+suma=n1+n2+n3+n4+n5
+resta=n1-n2-n3-n4-n5
+multi=n1*n2*n3*n4*n5
+divi=n1/n2/n3/n4/n5
+print("Su resultado es: ", str(suma))
+print("Su resultado es: ", str(resta))
+print("Su resultado es: ", str(multi)) 
+print("Su resultado es: ", str(divi))
